df5b5ffe40679dafa940ce50488c365fc1e2a7ce
alpa1479 <alpa1479@gmail.com>
2023-09-09 18:17:21 +0400

    [main] - added intermediate certificate to demonstrate work with certificate chain

--- examples/https-server/src/main/resources/scripts/generate-certificates.sh
+++ examples/https-server/src/main/resources/scripts/generate-certificates.sh
@@ -4,11 +4,13 @@ CERT_DIR='../certificates'
 rm -rf $CERT_DIR
 
 CA_CERT_DIR=$CERT_DIR/ca
+INTERMEDIATE_CERT_DIR=$CERT_DIR/intermediate
 SERVER_CERT_DIR=$CERT_DIR/server
 CLIENT_CERT_DIR=$CERT_DIR/client
 
 mkdir $CERT_DIR
 mkdir $CA_CERT_DIR
+mkdir $INTERMEDIATE_CERT_DIR
 mkdir $SERVER_CERT_DIR
 mkdir $CLIENT_CERT_DIR
 
@@ -22,12 +24,29 @@ openssl req -new -key $CA_CERT_DIR/ca-private-key.key -passin pass:$PASSWORD -su
 # Creation of CA certificate using CA CSR and CA private key
 openssl x509 -req -in $CA_CERT_DIR/ca-certificate-signing-request.csr -signkey $CA_CERT_DIR/ca-private-key.key -passin pass:$PASSWORD -days $EXPIRATION_TIME -out $CA_CERT_DIR/ca-self-signed-certificate.pem
 
+# Creation of intermediate private key
+openssl genrsa -aes256 -passout pass:$PASSWORD -out $INTERMEDIATE_CERT_DIR/intermediate-private-key.key 4096
+# Creation of intermediate CSR using intermediate private key
+openssl req -new -key $INTERMEDIATE_CERT_DIR/intermediate-private-key.key -passin pass:$PASSWORD -subj "//CN=intermediate" -out $INTERMEDIATE_CERT_DIR/intermediate-certificate-signing-request.csr
+# Creation of openssl.cnf config file to add constraint that this certificate may be used as CA certificate
+echo "[ v3_intermediate_ca ]" >>$INTERMEDIATE_CERT_DIR/openssl.cnf
+echo "basicConstraints = CA:TRUE" >>$INTERMEDIATE_CERT_DIR/openssl.cnf
+# Creation of intermediate certificate using intermediate CSR and CA private key with CA certificate
+openssl x509 -req -in $INTERMEDIATE_CERT_DIR/intermediate-certificate-signing-request.csr -CA $CA_CERT_DIR/ca-self-signed-certificate.pem -CAkey $CA_CERT_DIR/ca-private-key.key -passin pass:$PASSWORD -extfile $INTERMEDIATE_CERT_DIR/openssl.cnf -extensions v3_intermediate_ca -CAcreateserial -days $EXPIRATION_TIME -out $INTERMEDIATE_CERT_DIR/intermediate-certificate.pem
+
 # Creation of server private key
 openssl genrsa -aes256 -passout pass:$PASSWORD -out $SERVER_CERT_DIR/server-private-key.key 4096
 # Creation of server CSR using server private key
 openssl req -new -key $SERVER_CERT_DIR/server-private-key.key -passin pass:$PASSWORD -subj "//CN=localhost" -out $SERVER_CERT_DIR/server-certificate-signing-request.csr
-# Creation of server certificate using server CSR and server private key with CA certificate
-openssl x509 -req -in $SERVER_CERT_DIR/server-certificate-signing-request.csr -CA $CA_CERT_DIR/ca-self-signed-certificate.pem -CAkey $CA_CERT_DIR/ca-private-key.key -passin pass:$PASSWORD -CAcreateserial -days $EXPIRATION_TIME -out $SERVER_CERT_DIR/server-certificate.pem
+# Creation of server certificate using server CSR and intermediate private key with intermediate certificate
+openssl x509 -req -in $SERVER_CERT_DIR/server-certificate-signing-request.csr -CA $INTERMEDIATE_CERT_DIR/intermediate-certificate.pem -CAkey $INTERMEDIATE_CERT_DIR/intermediate-private-key.key -passin pass:$PASSWORD -CAcreateserial -days $EXPIRATION_TIME -out $SERVER_CERT_DIR/server-certificate.pem
+
+# Creation of server certificate chain
+cat $INTERMEDIATE_CERT_DIR/intermediate-certificate.pem >>$SERVER_CERT_DIR/server-certificate-chain.pem
+cat $SERVER_CERT_DIR/server-certificate.pem >>$SERVER_CERT_DIR/server-certificate-chain.pem
+
+# Creation of keystore with server certificate and private key
+openssl pkcs12 -export -in $SERVER_CERT_DIR/server-certificate-chain.pem -inkey $SERVER_CERT_DIR/server-private-key.key -passin pass:$PASSWORD -passout pass:$PASSWORD -name server -out $CERT_DIR/keystore.p12
 
 # Creation of client private key
 openssl genrsa -aes256 -passout pass:$PASSWORD -out $CLIENT_CERT_DIR/client-private-key.key 4096
@@ -36,8 +55,11 @@ openssl req -new -key $CLIENT_CERT_DIR/client-private-key.key -passin pass:$PASS
 # Creation of client certificate using client CSR and client private key with CA certificate
 openssl x509 -req -in $CLIENT_CERT_DIR/client-certificate-signing-request.csr -CA $CA_CERT_DIR/ca-self-signed-certificate.pem -CAkey $CA_CERT_DIR/ca-private-key.key -passin pass:$PASSWORD -days $EXPIRATION_TIME -out $CLIENT_CERT_DIR/client-certificate.pem
 
-# Creation of keystore with server certificate and private key
-openssl pkcs12 -export -in $SERVER_CERT_DIR/server-certificate.pem -inkey $SERVER_CERT_DIR/server-private-key.key -passin pass:$PASSWORD -passout pass:$PASSWORD -name server -out $CERT_DIR/keystore.p12
-
 # Creation of truststore with CA certificate
 keytool -import -file $CA_CERT_DIR/ca-self-signed-certificate.pem -keystore $CERT_DIR/truststore.p12 -storetype PKCS12 -storepass $PASSWORD -noprompt -alias ca
+
+# Utility commands:
+# ./generate-certificates.sh
+# openssl verify -CAfile ../certificates/ca/ca-self-signed-certificate.pem ../certificates/server/server-certificate-chain.pem
+# openssl x509 -noout -text -in ../certificates/ca/ca-self-signed-certificate.pem
+# openssl x509 -noout -text -in ../certificates/intermediate/intermediate-certificate.pem
